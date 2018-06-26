e1faf05b5576d758f420b12e00971e3b47619952
fred <frede.staal@hotmail.dk>
2018-06-26 15:54:43 +0200

    d

---
+++ assets/js/javascript.js
@@ -0,0 +1,12 @@
+function projects() {
+    var scroll = document.querySelector("#projects");
+    scroll.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+}
+function about() {
+    var scroll = document.querySelector("#aboutme");
+    scroll.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+}
+function contact() {
+    var scroll = document.querySelector("#contact");
+    scroll.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+}
